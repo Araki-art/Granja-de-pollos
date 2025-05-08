b3234ac916ec163921e5560fc03ff6fdbd035ae4
Araki-art <arakimododiavlo@gmail.com>
2025-05-08 11:13:01 -0500

    v2
    cambie el nombre "BD" por "Base_datos"

---
+++ Granja de pollos/Base_datos.py
@@ -0,0 +1,13 @@
+class Base_datos:
+    def __init__(self):
+        self.lista_datos =[["P001" , "pequeña" , "15dias"]]
+
+    def imprimir_info(self):
+        for self.lista_datos in range(self.lista_datos.len):
+            for (info_pollo in self.lista_datos)
+            print(info_pollo)
+
+    def crear_pollo
+    def leer_pollo
+    def actualizar_pollo 
+    def eliminar_pollo 
--- Granja de pollos/Pollos.py
+++ Granja de pollos/Pollos.py
@@ -1,1 +1,26 @@
-dir 
+class Pollos:
+    #metodos contructores
+    def __init__(self):
+        self.codigo_pollo = id_pollo
+        self.edad_pollo = dato_edad
+        
+    #llamados de otras clases 
+    self_objBase_datos = base_datos() #el objeto de la clase 
+
+
+
+    #metodos publicos de modificar atributos 
+    def getCodigo_pollo (self):
+        return self.codigo_pollo
+
+    def setCodigo_pollo (self , codigo_pollo):
+        self.codigo_pollo = codigo_pollo
+
+    #metodos para conextar base de datos
+    def guardar_pollo(self):
+        self.self_objBase_datos.guardar_pollo(obj_pollo)
+        
+
+
+        
+

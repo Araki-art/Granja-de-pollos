bba81fa7fe92aea31833a707018d8d499f6b1913
Araki-art <arakimododiavlo@gmail.com>
2025-05-14 17:18:36 -0500

    Update Base_datos.py
    v3

--- Granja de pollos/Base_datos.py
+++ Granja de pollos/Base_datos.py
@@ -1,13 +1,31 @@
 class Base_datos:
-    def __init__(self):
-        self.lista_datos =[["P001" , "pequeña" , "15dias"]]
+  def __init__(self):
+    self.lista_datos = [["P001", "pequeña", "15dias"]]
 
-    def imprimir_info(self):
-        for self.lista_datos in range(self.lista_datos.len):
-            for (info_pollo in self.lista_datos)
-            print(info_pollo)
+  def imprimir_info(self):
+    for info_pollo in self.lista_datos:
+      print(info_pollo)
 
-    def crear_pollo
-    def leer_pollo
-    def actualizar_pollo 
-    def eliminar_pollo 
+  def crear_pollo(self, pollo):
+    nuevo = [pollo.codigo_pollo, "desconocido", f"{pollo.edad_pollo}dias"]
+    self.lista_datos.append(nuevo)
+
+  def leer_pollo(self, codigo):
+    for pollo in self.lista_datos:
+      if pollo[0] == codigo:
+        return pollo
+    return None
+
+  def actualizar_pollo(self, codigo, nueva_edad):
+    for pollo in self.lista_datos:
+      if pollo[0] == codigo:
+        pollo[2] = nueva_edad
+        return True
+    return False
+
+  def eliminar_pollo(self, codigo):
+    for i, pollo in enumerate(self.lista_datos):
+      if pollo[0] == codigo:
+        del self.lista_datos[i]
+        return True
+    return False

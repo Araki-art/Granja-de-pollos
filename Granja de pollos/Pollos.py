18fb202a48a5af25785241548be87f76b2467fe8
Araki-art <arakimododiavlo@gmail.com>
2025-05-14 21:59:33 -0500

    Update Pollos.py
    v4

--- Granja de pollos/Pollos.py
+++ Granja de pollos/Pollos.py
@@ -1,33 +1,42 @@
-from Base_datos import Base_datos  
+from Base_datos import Base_datos
 
 class Pollos:
-    # métodos constructores
-    def __init__(self, id_pollo, dato_edad):
-        self.codigo_pollo = id_pollo
-        self.edad_pollo = dato_edad
+  # métodos constructores
+  def __init__(self, id_pollo, dato_raza, dato_edad, produccion):
+    self.codigo_pollo = id_pollo
+    self.raza_pollo = dato_raza
+    self.edad_pollo = dato_edad
+    self.produccion_semanal = produccion
+    self.objBase_datos = Base_datos()
 
-        # objeto de la base de datos
-        self.self_objBase_datos = Base_datos()
+  # métodos públicos para modificar atributos
+  def getCodigo_pollo(self):
+    return self.codigo_pollo
 
-    # métodos públicos para modificar atributos
-    def getCodigo_pollo(self):
-        return self.codigo_pollo
+  def setCodigo_pollo(self, codigo_pollo):
+    self.codigo_pollo = codigo_pollo
 
-    def setCodigo_pollo(self, codigo_pollo):
-        self.codigo_pollo = codigo_pollo
+  def getRaza_pollo(self):
+    return self.raza_pollo
 
-    def getEdad_pollo(self):  
-        return self.edad_pollo
+  def setRaza_pollo(self, raza_pollo):
+    self.raza_pollo = raza_pollo
 
-    def setEdad_pollo(self, edad_pollo): 
-        self.edad_pollo = edad_pollo
+  def getEdad_pollo(self):
+    return self.edad_pollo
 
-    # métodos para conectar base de datos
-    def guardar_pollo(self):
-        obj_pollo = [self.codigo_pollo, "desconocida", self.edad_pollo]  
-        self.self_objBase_datos.crear_pollo(obj_pollo) 
+  def setEdad_pollo(self, edad_pollo):
+    self.edad_pollo = edad_pollo
 
+  def getProduccion_semanal(self):
+    return self.produccion_semanal
 
+  def setProduccion_semanal(self, produccion):
+    self.produccion_semanal = produccion
+
+  # métodos para conectar base de datos
+  def guardar_pollo(self):
+    self.objBase_datos.crear_pollo(self)
 
         
 
